ebf732c6ab2e6fefc8de1fd601dbb60d689b07e8
harshxraj <harshraj0588@gmail.com>
2024-01-05 15:41:26 +0530

    D

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,5 @@
+import React from "react";
 
-import React from 'react'
-
-export const  HomePage = () => {
-  return (
-    <div>
-      HomePage
-    </div>
-  )
-}
-
+export const HomePage = () => {
+  return <div>HomePage</div>;
+};
--- src/Routes/privateRoute.jsx
+++ src/Routes/privateRoute.jsx
@@ -2,14 +2,10 @@ import { useSelector } from "react-redux";
 import { Navigate, useNavigate } from "react-router-dom";
 
 export const PrivateRoute = ({ children }) => {
-    const isAuth =true
-    const navigate = useNavigate();
-    if (!isAuth) {
-        return <Navigate to="/login" />
-    }
-    return (
-        <>
-            {children}
-        </>
-    )
+  const isAuth = true;
+  const navigate = useNavigate();
+  if (!isAuth) {
+    return <Navigate to="/login" />;
+  }
+  return <>{children}</>;
 };
